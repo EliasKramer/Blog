a2b0914147f6c8a3cf06fecd9e93e244a0ff4c0b
Elias Kramer <67228319+EliasKramer@users.noreply.github.com>
2023-02-22 11:00:18 +0100

    added card animation

--- src/app/projects/project-card/project-card.component.ts
+++ src/app/projects/project-card/project-card.component.ts
@@ -1,11 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Project } from 'src/app/data/areas-of-interest/project';
+import { TerminalComponent } from 'src/app/main/terminal/terminal.component';
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss']
 })
-export class ProjectCardComponent {
+export class ProjectCardComponent implements OnInit {
   @Input() project: Project;
+  @Input() idx: number;
+
+  delay: number;
+
+  ngOnInit(): void {
+      this.delay = this.idx * 0.5;
+  }
 }
